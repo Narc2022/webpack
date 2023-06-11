23d5465d1865c7c812b9bb0f606f5239be05d90b
sachin <sachin.singh@azent.com>
2023-06-11 17:42:56 +0530

    last push

---
+++ Create a React App WITHOUT Create React App/src/components/App.js
@@ -0,0 +1,18 @@
+import React,{Component} from 'react'
+
+// const App = () => {
+//   return (
+//     <div>App</div>
+//   )
+// }
+
+class App extends Component {
+    render() {
+        return (
+            <h1>Hello World with webpack is here!</h1>
+        )
+
+    }
+}
+
+export default App
